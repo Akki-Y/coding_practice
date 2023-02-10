1533e028915e7e2b898c2f50b32dc4063c783e9b
Akki <akshayayathakula@gmail.com>
2023-02-10 13:07:41 +0530

    Examples of Loops

---
+++ learning/Ruby/Loops/Examples/while.rb
@@ -0,0 +1,38 @@
+# #While - It executes the code repeatedly as long as condition is true.
+# #new line-backslash\  or semicolon:
+a=0
+b=6
+while a < b 
+   print a,".condition is true \n"   
+    a+=1 # no of iterations
+end
+
+
+# # puts- cursor moves to next line and prints output in new line (by default)
+# # print -  cursor  doesnot moves to next line and prints output in new line (by default)
+
+# # using while begin end
+# # a = 0
+# # b = 6
+
+# # begin 
+# # #     puts "inside loop  a = #{a} "
+# #     a +=1
+# # end while a < b
+
+
+i=0
+num =5
+while i < num 
+puts (" True = #{i}")
+i+=1
+end 
+
+
+
+numbers = [10, 20, 30, 40, 50]
+# greatest_num = numbers.first
+# for n in numbers
+#     greatest_num = n if n > greatest_num
+# end
+# p greatest_num
